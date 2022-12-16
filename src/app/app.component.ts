8b6951d246ab5bc53b6bc4d9a3f9837d433c22c4
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-12-15 21:31:13 -0300

    add nuevo-componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NuevoComponenteComponent } from './nuevo-componente/nuevo-componente.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
+  
+  
 })
 export class AppComponent {
   title = 'proyecto-angularrr';
