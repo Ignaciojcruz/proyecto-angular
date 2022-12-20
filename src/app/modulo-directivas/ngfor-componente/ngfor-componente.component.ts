114ff5e385c0d38140217edacd8a335ea8993cec
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-12-20 16:29:15 -0300

    add ngFor component

--- src/app/modulo-directivas/ngfor-componente/ngfor-componente.component.ts
+++ src/app/modulo-directivas/ngfor-componente/ngfor-componente.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface PreguntasInterface {
+  pregunta: string;
+  si: number;
+  no: number;
+}
+
 @Component({
   selector: 'app-ngfor-componente',
   templateUrl: './ngfor-componente.component.html',
@@ -13,9 +19,41 @@ export class NgforComponenteComponent implements OnInit{
     "¿Estás aprendiendo Angular 2 en DesarrolloWeb?",
     "¿Has hecho ya algún curso en EscuelaIT?"
   ]
+
+  // preguntasObj = [
+  //   {
+  //     pregunta: "¿España ganará la Euro 2016?",
+  //     si: 22,
+  //     no: 95
+  //   },
+  //   {
+  //     pregunta: "¿Estás aprendiendo Angular 2 en DesarrolloWeb??",
+  //     si: 262,
+  //     no: 3
+  //   },
+  //   {
+  //     pregunta: "¿Has hecho ya algún curso en EscuelaIT??",
+  //     si: 1026,
+  //     no: 1
+  //   }
+  // ]
+
+  preguntasObj: PreguntasInterface[] = [
+    {
+      pregunta: "¿Te gusta usar interfaces?",
+      si: 72,
+      no:6
+    }
+  ]
+
+  
+
+  
   
   ngOnInit() {
     
   }
 
 }
+
+
